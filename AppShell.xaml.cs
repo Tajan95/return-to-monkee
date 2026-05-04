b06decb72cf55cb27b672425842d86b880f99683
Alex-Schiebelhut <alexschiebelhut28@gmail.com>
2026-05-04 15:00:19 +0200

    Add bare-bones .NET MAUI Hello World project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReturnToMonkee;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReturnToMonkee;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
